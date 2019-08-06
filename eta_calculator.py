a26fa4a95fca57f044911fd9c3b91895b61739dc
benmagoo808 <benmagoo@gmail.com>
2019-08-05 18:12:26 -0700

    Converted Dictionary DistanceNB from named variables to zip from two lists

--- eta_calculator.py
+++ eta_calculator.py
@@ -8,7 +8,34 @@ corresponding value.
 # Import from datetime to handle the time adjustments
 from datetime import datetime, timedelta
 
+WayPoints = ['Seattle', 'West Point', 'Point No Point', 'Bush Point',
+             'Point Partridge', 'Burrows Island', 'Lawrence Point',
+             'Patos Island', 'Ferry Crossing', 'Sand Heads', 'Entrance Island',
+             'Merry Island', 'Cape Lazo', 'Cape Mudge', 'Steep Island',
+             'Maude Island', 'Separation Head', 'Cinque Island', 'Ripple Point',
+             'Vansittart Point', 'Fanny Island', 'Boat Harbor Light',
+             'Lizard Point', 'Jeannette Island', 'Cape Caution', 'Dugout Rocks',
+             'Addenbroke Island', 'Fog Rocks', 'Pointer Island',
+             'Walker Island', 'Dryad Point', 'Idol Point', 'Ivory Island',
+             'Jorkins Point', 'Freeman Point', 'Boat Bluff', 'Ditmars Point',
+             'Sarah Head', 'Griffin Point', 'Work Island', 'Kingcome Point',
+             'Point Cumming', 'Sainty Point', 'Tom Island', 'Pitt Island',
+             'Baker Inlet', 'Watson Rock', 'Lawyer Island', 'Lucy Island',
+             'Holliday Island', 'Lord Rocks', 'Tree Point', 'Mary Island',
+             'Angle Point', 'Mountain Point', 'Ketchikan']
 
+Distances = [0, 5, 20.6, 29.9, 43.9, 60.5, 71.2, 85, 98, 107, 126.2, 139,
+             177.2, 199, 204.5, 209.5, 212.1, 219.2, 228, 234.2, 245.7, 268.1,
+             284.3, 309.1, 329.8, 342.9, 357.2, 379.6, 385.4, 392.9, 398.7,
+             405.8, 410.9, 424.4, 431.4, 437, 442.5, 452.1, 463, 471.6, 482.5,
+             490.5, 498.5, 513.5, 525.7, 533.9, 544.2, 557.7, 572.4, 593, 599.6,
+             605.6, 625.8, 638, 643.5, 648]
+
+# zip the lists into a dictionary
+DistanceNB = dict(zip(WayPoints, Distances))
+print(DistanceNB)
+
+'''
 # Define Waypoints as variables for future ease of use in other dicts and lists
 SeaHbr = 'Seattle Harbor'
 WesPt = 'West Point'
@@ -83,6 +110,8 @@ DistanceNB = {SeaHbr: 0, WesPt: 5, PtNoPt: 20.6, BushPt: 29.9, PtPtrg: 43.9,
               BakInl: 533.9, WatRk: 544.2, LwyrIsl: 557.7, LucIsl: 572.4,
               HolIsl: 593, LorRk: 599.6, TreePt: 605.6, MarIsl: 625.8,
               AngPt: 638, MntPt: 643.5, Ktn: 648}
+'''
+
 
 # Get speed and time
 print('What is your Estimated Speed in Knots?')
@@ -121,3 +150,8 @@ CalculatedETA = get_eta(EstimatedSpeed, EstimatedDeparture)
 # Convert datetime objects to strings and print as separate lines
 for k, v in CalculatedETA.items():
     print('You will arrive at ' + k + ' on ' + str(v))
+
+# Turn Keys into a list for better code
+
+Distances = DistanceNB.values()
+print(Distances)
