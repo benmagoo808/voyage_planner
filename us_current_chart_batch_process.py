59986debaae33f3665627cbcc457cc962fa13610
benmagoo808 <benmagoo@gmail.com>
2019-08-12 07:07:04 -0700

    fixed batch rename to use regex so can't corrupt filenames like slices could, need to finish csv header removal

--- us_current_chart_batch_process.py
+++ us_current_chart_batch_process.py
@@ -3,7 +3,7 @@ This script will batch process downloaded current charts from NOAA for us
 stations.
 It will rename the file using the following convention :
 <Station ID>_<year>.csv
-and remove the first row which is just titles for the values
+and remove the header row
 
 """
 
@@ -12,14 +12,35 @@ import os
 import re
 
 
-os.chdir('./current_tables')
+os.chdir('./current_tables')  # Cwd to current tables folder
+
+
+namePattern = re.compile(r'(\w\w\w\d\d\d\d).*(\d\d\d\d\.csv)', re.VERBOSE)
+
+
 for f in os.listdir('.'):
     if not f.startswith('.'):
-        stationID = f[:7]
-        year = f[-8:]
-        path = os.path.split(f)
+        name = namePattern.search(f)
+        stationID = name.group(1)
+        year = name.group(2)
         os.rename(f, stationID + '_' + year)
+        if not f.endswith('.csv'):
+            continue
 
+        csvRows = []
+        csvFileObj = open(f)
+        readerObj = csv.reader(csvFileObj)
+        for row in readerObj:
+            if readerObj.line_num == 1 and readerObj:
+                continue  # skip first row
+            csvRows.append(row)
+        csvFileObj.close()
 
-#rdr = csv.reader(open('./current_tables/PUG1515_15_Annual_2019.csv', 'rb'))
-#writer = csv.writer('./current_tables/PUG1515_2019.csv', 'wb')
+        # Write out the CSV file.
+        csvFileObj = open(os.path.join('headerRemoved', f), 'w',
+                          newline='')
+        csvWriter = csv.writer(csvFileObj)
+        for row in csvRows:
+            csvWriter.writerow(row)
+        csvFileObj.close()
+
