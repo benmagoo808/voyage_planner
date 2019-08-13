662e5e8ef90604aadc8241eae067dd9b1db9f69c
benmagoo808 <benmagoo@gmail.com>
2019-08-12 17:54:27 -0700

    completed batch edit script for current_tables folder

--- us_current_chart_batch_process.py
+++ us_current_chart_batch_process.py
@@ -1,9 +1,9 @@
 """
-This script will batch process downloaded current charts from NOAA for us
+This script will batch process downloaded current charts from NOAA for US
 stations.
 It will rename the file using the following convention :
 <Station ID>_<year>.csv
-and remove the header row
+and remove the header row from the csv file if one is present
 
 """
 
@@ -14,33 +14,40 @@ import re
 
 os.chdir('./current_tables')  # Cwd to current tables folder
 
+# Regex pattern pulls the groups we want from the downloaded format
+namePattern = re.compile(r'(\w\w\w\d\d\d\d).*(20\d\d).*(\.csv)',
+                         re.VERBOSE)
+yearPattern = re.compile(r'20\d\d.*')  # pattern for data instead of header
 
-namePattern = re.compile(r'(\w\w\w\d\d\d\d).*(\d\d\d\d\.csv)', re.VERBOSE)
 
-
-for f in os.listdir('.'):
-    if not f.startswith('.'):
-        name = namePattern.search(f)
+for f in os.listdir('.'):  # iterate through the files in dir
+    if not f.startswith('.'):  # unless they are hidden files
+        name = namePattern.search(f)  # grab the patterns we want from the name
         stationID = name.group(1)
         year = name.group(2)
-        os.rename(f, stationID + '_' + year)
-        if not f.endswith('.csv'):
-            continue
+        ext = name.group(3)
+        os.rename(f, stationID + '_' + year + ext)  # rename using our format
 
+
+for f in os.listdir('.'):  # iterate through the files in dir
+    if f.endswith('.csv'):  # that are .csv
         csvRows = []
-        csvFileObj = open(f)
-        readerObj = csv.reader(csvFileObj)
-        for row in readerObj:
-            if readerObj.line_num == 1 and readerObj:
-                continue  # skip first row
-            csvRows.append(row)
-        csvFileObj.close()
+        fileObj = open(f, 'r')  # open each file
+        reader = csv.reader(fileObj)  # read it with csv.reader
+        for row in reader:
+            year = yearPattern.search(row[0])  # check if first line is data
+            if year is None:  # if not, skip write to csvRows
+                continue
+            else:
+                csvRows.append(row)  # if data, write to csvRows
 
-        # Write out the CSV file.
-        csvFileObj = open(os.path.join('headerRemoved', f), 'w',
-                          newline='')
-        csvWriter = csv.writer(csvFileObj)
+        fileObj.close()  # close file
+
+        fileObj = open(f, 'w', newline='')  # open again in write mode
+        writer = csv.writer(fileObj)
         for row in csvRows:
-            csvWriter.writerow(row)
-        csvFileObj.close()
+            writer.writerow(row)  # write each row from csvRows to the file
+        fileObj.close()  # close file
 
+
+print('Batch editing complete!')
